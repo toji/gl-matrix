087969df1658b1c22f48b7db6fab237a2b48dbb5
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-11-28 18:09:04 -0500

    add tests for vec3.scale

--- spec/javascripts/vec3_spec.js
+++ spec/javascripts/vec3_spec.js
@@ -101,4 +101,25 @@ describe("vec3", function() {
       it("should modify dest vector", function() { expect(dest).toBeEqualish([-1,-2,-3]); });
     });
   });
+  
+  describe("scale", function() {
+    beforeEach(function() { vec = vec3.create([1,2,3]); });
+    
+    it("should modify original vector if dest not given", function() {
+      vec3.scale(vec, 2);
+      expect(vec).toBeEqualish([2,4,6]);
+    });
+    
+    it("should modify original vector if dest is original vector", function() {
+      vec3.scale(vec, 2, vec);
+      expect(vec).toBeEqualish([2,4,6]);
+    });
+    
+    describe("if dest vector given", function() {
+      beforeEach(function() { vec3.scale(vec, 2, dest = vec3.create()); });
+      
+      it("should not modify original vector", function() { expect(vec).toBeEqualish([1,2,3]); });
+      it("should modify dest vector", function() { expect(dest).toBeEqualish([2,4,6]); });
+    });
+  });
 });
