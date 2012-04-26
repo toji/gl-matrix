4d1d2292db90c949630055c6e8461653e556c4cc
Eduardo Poyart <epoyart@macbook>
2012-04-26 14:59:45 -0700

    Added mat3.multiply

--- gl-matrix.js
+++ gl-matrix.js
@@ -526,6 +526,40 @@ mat3.transpose = function (mat, dest) {
 };
 
 /**
+ * Performs a matrix multiplication
+ *
+ * @param {mat3} mat First operand
+ * @param {mat3} mat2 Second operand
+ * @param {mat3} [dest] mat3 receiving operation result. If not specified result is written to mat
+ *
+ * @returns {mat3} dest if specified, mat otherwise
+ */
+mat3.multiply = function (mat, mat2, dest) {
+    if (!dest) { dest = mat; }
+
+    // Cache the matrix values (makes for huge speed increases!)
+    var a00 = mat[0], a01 = mat[1], a02 = mat[2],
+        a10 = mat[3], a11 = mat[4], a12 = mat[5],
+        a20 = mat[6], a21 = mat[7], a22 = mat[8],
+
+        b00 = mat2[0], b01 = mat2[1], b02 = mat2[2],
+        b10 = mat2[3], b11 = mat2[4], b12 = mat2[5],
+        b20 = mat2[6], b21 = mat2[7], b22 = mat2[8];
+
+    dest[0] = b00 * a00 + b01 * a10 + b02 * a20;
+    dest[1] = b00 * a01 + b01 * a11 + b02 * a21;
+    dest[2] = b00 * a02 + b01 * a12 + b02 * a22;
+    dest[3] = b10 * a00 + b11 * a10 + b12 * a20;
+    dest[4] = b10 * a01 + b11 * a11 + b12 * a21;
+    dest[5] = b10 * a02 + b11 * a12 + b12 * a22;
+    dest[6] = b20 * a00 + b21 * a10 + b22 * a20;
+    dest[7] = b20 * a01 + b21 * a11 + b22 * a21;
+    dest[8] = b20 * a02 + b21 * a12 + b22 * a22;
+
+    return dest;
+};
+
+/**
  * Copies the elements of a mat3 into the upper 3x3 elements of a mat4
  *
  * @param {mat3} mat mat3 containing values to copy
