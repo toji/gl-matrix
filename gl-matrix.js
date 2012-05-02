5a3d7740caf29ea7af68818634417bf663217454
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-05-02 16:22:28 -0400

    bugfix in mat3.multiplyVec3 when not given a dest vec3

--- gl-matrix.js
+++ gl-matrix.js
@@ -539,10 +539,10 @@
      **/
     mat3.multiplyVec3 = function(matrix, vec, dest) {
       if (!dest) dest = vec;
-      
-      dest[0] = vec[0] * matrix[0] + vec[1] * matrix[3] + vec[2] * matrix[6];
-      dest[1] = vec[0] * matrix[1] + vec[1] * matrix[4] + vec[2] * matrix[7];
-      dest[2] = vec[0] * matrix[2] + vec[1] * matrix[5] + vec[2] * matrix[8];
+      var x = vec[0], y = vec[1], z = vec[2];
+      dest[0] = x * matrix[0] + y * matrix[3] + z * matrix[6];
+      dest[1] = x * matrix[1] + y * matrix[4] + z * matrix[7];
+      dest[2] = x * matrix[2] + y * matrix[5] + z * matrix[8];
       
       return dest;
     };
