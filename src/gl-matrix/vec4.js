0c5742b510ee5e133f0e1c421ebf1f482fab593e
Nick Brancaccio <nick@floored.com>
2014-04-14 23:13:05 -0400

    Added component-wise inverse function.

--- src/gl-matrix/vec4.js
+++ src/gl-matrix/vec4.js
@@ -351,6 +351,21 @@ vec4.negate = function(out, a) {
 };
 
 /**
+ * Returns the inverse of the components of a vec4
+ *
+ * @param {vec4} out the receiving vector
+ * @param {vec4} a vector to invert
+ * @returns {vec4} out
+ */
+vec4.inverse = function(out, a) {
+  out[0] = 1.0 / a[0];
+  out[1] = 1.0 / a[1];
+  out[2] = 1.0 / a[2];
+  out[3] = 1.0 / a[3];
+  return out;
+};
+
+/**
  * Normalize a vec4
  *
  * @param {vec4} out the receiving vector
