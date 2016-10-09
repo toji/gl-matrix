15d806c71c191092a167a664ed0606a7aa1d55db
yiminghu <yiminghu@hustunique.com>
2016-10-09 11:52:09 +0800

    add getScaling function for mat4

--- src/gl-matrix/mat4.js
+++ src/gl-matrix/mat4.js
@@ -1501,6 +1501,34 @@ mat4.getTranslation = function (out, mat) {
 };
 
 /**
+ * Returns the scaling factor component of a transformation
+ *  matrix. If a matrix is built with fromRotationTranslationScale
+ *  with a normalized Quaternion paramter, the returned vector will be 
+ *  the same as the scaling vector
+ *  originally supplied.
+ * @param  {vec3} out Vector to receive scaling factor component
+ * @param  {mat4} mat Matrix to be decomposed (input)
+ * @return {vec3} out
+ */
+mat4.getScaling = function (out, mat) {
+  var m11 = mat[0],
+      m12 = mat[1],
+      m13 = mat[2],
+      m21 = mat[4],
+      m22 = mat[5],
+      m23 = mat[6],
+      m31 = mat[8],
+      m32 = mat[9],
+      m33 = mat[10];
+
+  out[0] = Math.sqrt(m11 * m11 + m12 * m12 + m13 * m13);
+  out[1] = Math.sqrt(m21 * m21 + m22 * m22 + m23 * m23);
+  out[2] = Math.sqrt(m31 * m31 + m32 * m32 + m33 * m33);
+
+  return out;
+};
+
+/**
  * Returns a quaternion representing the rotational component
  *  of a transformation matrix. If a matrix is built with
  *  fromRotationTranslation, the returned quaternion will be the
