47f1774f9ef5333a61a278e13cf2ba0533a08986
Artjom <artjomvinnikov@gmail.com>
2017-08-09 20:46:30 +0300

    fix lookAt, mat4 is not defined

--- src/gl-matrix/mat4.js
+++ src/gl-matrix/mat4.js
@@ -1367,7 +1367,7 @@ export function lookAt(out, eye, center, up) {
   if (Math.abs(eyex - centerx) < glMatrix.EPSILON &&
       Math.abs(eyey - centery) < glMatrix.EPSILON &&
       Math.abs(eyez - centerz) < glMatrix.EPSILON) {
-    return mat4.identity(out);
+    return out;
   }
 
   z0 = eyex - centerx;
