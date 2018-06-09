071eb145e4978bc498b68e13a6116ac9f5bd9e1b
d07RiV <d07.riv@gmail.com>
2018-06-09 13:03:27 +0300

    add perspectiveInfinite

--- spec/gl-matrix/mat4-spec.js
+++ spec/gl-matrix/mat4-spec.js
@@ -660,6 +660,29 @@ function buildMat4Tests() {
             });
         });
 
+        describe("perspectiveInfinite", function() {
+            let fovy = Math.PI * 0.5;
+            beforeEach(function() { result = mat4.perspectiveInfinite(out, fovy, 1, 0); });
+            it("should place values into out", function() { expect(result).toBeEqualish([
+                    1, 0, 0, 0,
+                    0, 1, 0, 0,
+                    0, 0, -1, -1,
+                    0, 0, 0, 0
+                ]);
+            });
+            it("should return out", function() { expect(result).toBe(out); });
+
+            describe("with nonzero near, 45deg fovy, and realistic aspect ratio", function() {
+                beforeEach(function() { result = mat4.perspectiveInfinite(out, 45 * Math.PI / 180.0, 640/480, 0.1); });
+                it("should calculate correct matrix", function() { expect(result).toBeEqualish([
+                    1.81066, 0, 0, 0,
+                    0, 2.414213, 0, 0,
+                    0, 0, -1, -1,
+                    0, 0, -0.2, 0
+                ]); });
+            });
+        });
+
         describe("ortho", function() {
             beforeEach(function() { result = mat4.ortho(out, -1, 1, -1, 1, -1, 1); });
             it("should place values into out", function() { expect(result).toBeEqualish([
--- src/gl-matrix/mat4.js
+++ src/gl-matrix/mat4.js
@@ -1288,6 +1288,36 @@ export function perspective(out, fovy, aspect, near, far) {
 }
 
 /**
+ * Generates a perspective projection matrix without far plane
+ *
+ * @param {mat4} out mat4 frustum matrix will be written into
+ * @param {number} fovy Vertical field of view in radians
+ * @param {number} aspect Aspect ratio. typically viewport width/height
+ * @param {number} near Near bound of the frustum
+ * @returns {mat4} out
+ */
+export function perspectiveInfinite(out, fovy, aspect, near) {
+  let f = 1.0 / Math.tan(fovy / 2);
+  out[0] = f / aspect;
+  out[1] = 0;
+  out[2] = 0;
+  out[3] = 0;
+  out[4] = 0;
+  out[5] = f;
+  out[6] = 0;
+  out[7] = 0;
+  out[8] = 0;
+  out[9] = 0;
+  out[10] = -1;
+  out[11] = -1;
+  out[12] = 0;
+  out[13] = 0;
+  out[14] = -2 * near;
+  out[15] = 0;
+  return out;
+}
+
+/**
  * Generates a perspective projection matrix with the given field of view.
  * This is primarily useful for generating projection matrices to be used
  * with the still experiemental WebVR API.
