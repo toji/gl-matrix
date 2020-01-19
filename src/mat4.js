93908bee6d86db67e8e6b63b3b3e3870cc0aec29
Luke Williams <shmookey@shmookey.net>
2020-01-19 13:54:58 +0800

    Fix missing element in Frobenius norm for mat4

--- src/mat4.js
+++ src/mat4.js
@@ -1552,7 +1552,7 @@ export function str(a) {
  * @returns {Number} Frobenius norm
  */
 export function frob(a) {
-  return(Math.hypot(a[0],a[1],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15]))
+  return(Math.hypot(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15]))
 }
 
 /**
