83b4ced4aaa859273d0cabc24dfc46fbe274ec01
Brandon Jones <tojiro@gmail.com>
2024-12-17 07:46:11 -0800

    Update normalFromMat4 variants

--- src/_lib/f32/Mat3.ts
+++ src/_lib/f32/Mat3.ts
@@ -777,106 +777,48 @@ export class Mat3 extends Float32Array {
   }
 
   /**
-   * Calculates a 3x3 normal matrix (transpose inverse) from the 4x4 matrix
+   * Calculates a {@link Mat3} normal matrix (adjoint) from the upper 3x3 of a {@link Mat4}.
+   * See https://www.shadertoy.com/view/3s33zj for details.
    * @category Static
    *
    * @param {mat3} out mat3 receiving operation result
    * @param {ReadonlyMat4} a Mat4 to derive the normal matrix from
    * @returns `out` or `null` if the matrix is not invertible
    */
-  static normalFromMat4(out: Mat3Like, a: Readonly<Mat4Like>): Mat3Like | null {
+  static normalFromMat4(out: Mat3Like, a: Readonly<Mat4Like>): Mat3Like {
+    // Only difference from adjoint() is these indices.
     const a00 = a[0];
     const a01 = a[1];
     const a02 = a[2];
-    const a03 = a[3];
+
     const a10 = a[4];
     const a11 = a[5];
     const a12 = a[6];
-    const a13 = a[7];
+
     const a20 = a[8];
     const a21 = a[9];
     const a22 = a[10];
-    const a23 = a[11];
-    const a30 = a[12];
-    const a31 = a[13];
-    const a32 = a[14];
-    const a33 = a[15];
 
-    const b00 = a00 * a11 - a01 * a10;
-    const b01 = a00 * a12 - a02 * a10;
-    const b02 = a00 * a13 - a03 * a10;
-    const b03 = a01 * a12 - a02 * a11;
-    const b04 = a01 * a13 - a03 * a11;
-    const b05 = a02 * a13 - a03 * a12;
-    const b06 = a20 * a31 - a21 * a30;
-    const b07 = a20 * a32 - a22 * a30;
-    const b08 = a20 * a33 - a23 * a30;
-    const b09 = a21 * a32 - a22 * a31;
-    const b10 = a21 * a33 - a23 * a31;
-    const b11 = a22 * a33 - a23 * a32;
+    out[0] = a11 * a22 - a12 * a21;
+    out[1] = a02 * a21 - a01 * a22;
+    out[2] = a01 * a12 - a02 * a11;
 
-    // Calculate the determinant
-    let det =
-      b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+    out[3] = a12 * a20 - a10 * a22;
+    out[4] = a00 * a22 - a02 * a20;
+    out[5] = a02 * a10 - a00 * a12;
 
-    if (!det) {
-      return null;
-    }
-    det = 1.0 / det;
-
-    out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
-    out[1] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
-    out[2] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
-
-    out[3] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
-    out[4] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
-    out[5] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
-
-    out[6] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
-    out[7] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
-    out[8] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
-
+    out[6] = a10 * a21 - a11 * a20;
+    out[7] = a01 * a20 - a00 * a21;
+    out[8] = a00 * a11 - a01 * a10;
     return out;
   }
 
   /**
-   * Calculates a {@link Mat3} normal matrix (transpose inverse) from a {@link Mat4}
-   * This version omits the calculation of the constant factor (1/determinant), so
-   * any normals transformed with it will need to be renormalized.
-   * From https://stackoverflow.com/a/27616419/25968
+   * Alias for {@link Mat3.adjointFromMat4}
    * @category Static
-   *
-   * @param out - Matrix receiving operation result
-   * @param a - Mat4 to derive the normal matrix from
-   * @returns `out`
+   * @deprecated Use {@link Mat3.normalFromMat4}
    */
-  static normalFromMat4Fast(out: Mat3Like, a: Readonly<Mat4Like>): Mat3Like {
-    const ax = a[0];
-    const ay = a[1];
-    const az = a[2];
-
-    const bx = a[4];
-    const by = a[5];
-    const bz = a[6];
-
-    const cx = a[8];
-    const cy = a[9];
-    const cz = a[10];
-
-    out[0] = by * cz - cz * cy;
-    out[1] = bz * cx - cx * cz;
-    out[2] = bx * cy - cy * cx;
-
-    out[3] = cy * az - cz * ay;
-    out[4] = cz * ax - cx * az;
-    out[5] = cx * ay - cy * ax;
-
-    out[6] = ay * bz - az * by;
-    out[7] = az * bx - ax * bz;
-    out[8] = ax * by - ay * bx;
-
-    return out;
-  }
+  static normalFromMat4Fast(out: Mat3Like, a: Readonly<Mat4Like>): Mat3Like { return out; }
 
   /**
    * Generates a 2D projection matrix with the given bounds
@@ -1048,3 +990,4 @@ Mat3.prototype.mul = Mat3.prototype.multiply; // eslint-disable-line @typescript
 // Static method alias assignments
 Mat3.mul = Mat3.multiply;
 Mat3.sub = Mat3.subtract;
+Mat3.normalFromMat4Fast = Mat3.normalFromMat4;
